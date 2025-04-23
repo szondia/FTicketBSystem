553f7507a703888deca60cdf049abc7fb08987ba
szondia <89350722+szondia@users.noreply.github.com>
2025-04-23 20:00:48 +0200

    Update Legitarsasag.py
    Kód egyszerűsités

--- Legitarsasag.py
+++ Legitarsasag.py
@@ -1,16 +1,19 @@
 class LegiTarsasag:
     def __init__(self, nev):
         self.nev = nev
-        self.jaratok = []
+        self.jaratok = []  # lista Jarat objektumokkal
 
     def hozzaad_jarat(self, jarat):
         """Hozzáad egy járatot a légitársasághoz."""
         self.jaratok.append(jarat)
 
     def get_elerheto_jaratok(self):
-        """Visszaadja az elérhető (még foglalható) járatok listáját."""
-        return [j for j in self.jaratok if j.elerheto]
+        """Visszaadja az összes járatot."""
+        return self.jaratok
 
     def find_jarat(self, jaratszam):
         """Keres egy járatot a járatszám alapján."""
-        return next((j for j in self.jaratok if j.jaratszam == jaratszam), None)
+        for j in self.jaratok:
+            if j.jaratszam == jaratszam:
+                return j
+        return None
