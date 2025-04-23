b4dea3dc483b16426b4da14d52e1732e92f7d0d7
szondia <89350722+szondia@users.noreply.github.com>
2025-04-23 21:47:31 +0200

    Final Legitarsasag.py

--- Legitarsasag.py
+++ Legitarsasag.py
@@ -1,18 +1,15 @@
 class LegiTarsasag:
     def __init__(self, nev):
         self.nev = nev
-        self.jaratok = []  # lista Jarat objektumokkal
+        self.jaratok = []
 
     def hozzaad_jarat(self, jarat):
-        """Hozzáad egy járatot a légitársasághoz."""
         self.jaratok.append(jarat)
 
     def get_elerheto_jaratok(self):
-        """Visszaadja az összes járatot."""
         return self.jaratok
 
     def find_jarat(self, jaratszam):
-        """Keres egy járatot a járatszám alapján."""
         for j in self.jaratok:
             if j.jaratszam == jaratszam:
                 return j
