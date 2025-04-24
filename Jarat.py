9a95f71b579ef63fedb738a23841ff069832a19c
szondia <89350722+szondia@users.noreply.github.com>
2025-04-24 10:52:17 +0200

    Update Jarat.py
    Attributomok kiegészitése (dátumok)

--- Jarat.py
+++ Jarat.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
+from datetime import date, time
 
 class Jarat(ABC):
-    def __init__(self, jaratszam, celallomas, jegyar):
+    def __init__(self, jaratszam: str, origin: str, destination: str, jegyar: float,
+                 departure_date: date, departure_time: time):
         self.jaratszam = jaratszam
-        self.celallomas = celallomas
+        self.origin = origin
+        self.destination = destination
         self.jegyar = jegyar
+        self.departure_date = departure_date
+        self.departure_time = departure_time
 
     @abstractmethod
-    def get_jegy_ara(self):
-        """
-        Visszaadja a jegy árát járattípus szerint módosítva.
-        """
+    def get_jegy_ara(self) -> float:
         pass
