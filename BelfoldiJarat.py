d8aa9ce519d4cdbe23e3a5353f7c8b4e7a1eeb5a
szondia <89350722+szondia@users.noreply.github.com>
2025-04-24 10:51:07 +0200

    Update BelfoldiJarat.py

--- BelfoldiJarat.py
+++ BelfoldiJarat.py
@@ -1,6 +1,5 @@
 from Jarat import Jarat
 
 class BelfoldiJarat(Jarat):
-    def get_jegy_ara(self):
-        # Belföldi járatok 10%-kal olcsóbbak
+    def get_jegy_ara(self) -> float:
         return self.jegyar * 0.9
